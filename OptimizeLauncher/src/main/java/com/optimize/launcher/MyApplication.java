b6f9c81bfa0552ba7962ba27a47fc1acda5b99a5
Ruibing.han <hanrb26@163.com>
2018-03-18 14:22:08 +0800

    Initial commit

---
+++ OptimizeLauncher/src/main/java/com/optimize/launcher/MyApplication.java
@@ -0,0 +1,19 @@
+package com.optimize.launcher;
+
+import android.app.Application;
+
+/**
+ * Created by Ruibing.han on 2018/3/17.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            Thread.sleep(800);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
