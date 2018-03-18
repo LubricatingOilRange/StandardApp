416ba6d78254cda2507e9571c72ee0d2b5c771fb
Ruibing.han <hanrb26@163.com>
2018-03-18 14:24:21 +0800

    .....

--- OptimizeLauncher/src/main/java/com/optimize/launcher/MyApplication.java
+++ OptimizeLauncher/src/main/java/com/optimize/launcher/MyApplication.java
@@ -10,6 +10,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
         try {
             Thread.sleep(800);
         } catch (InterruptedException e) {
